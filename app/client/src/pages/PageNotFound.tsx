357ef304b53329fd6f66f3ed825dba01f3e6f036
Lilit_Khalatyan <lkhalatyan@gmail.com>
2023-02-20 18:15:43 +0400

    add Page Not Found

--- app/client/src/pages/PageNotFound.tsx
+++ app/client/src/pages/PageNotFound.tsx
@@ -3,7 +3,7 @@ import warning from '../asset/images//warning/warning.svg';
 
 const PageNotFound: React.FC = () => {
 
-	return <LayoutWrapper><h4>Page Not Found</h4><img src={warning} alt="Access Denied" /></LayoutWrapper>
+	return <LayoutWrapper><h4>Page Not Found</h4><img src={warning} alt="Page Not Found" /></LayoutWrapper>
 };
 
 export default PageNotFound;
